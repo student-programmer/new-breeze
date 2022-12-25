02e7d25a5cfd9d257a1516c47c3536eaa9cb272c
Roman Podobny <tazer16179@gmail.com>
2022-12-25 14:34:39 +0300

    new commit

---
+++ client/pages/task.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import MainContainer from '../components/MainContainer';
+
+export default function Task() {
+    return (
+        <MainContainer keywords={'hello'} title='Обработка заявок'>
+            <div>Обработка заявок</div>
+        </MainContainer>
+    );
+}
